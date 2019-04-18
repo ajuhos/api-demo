9dffac51a7188905a9f852c93f2278ace9f7e858
Juhos Ádám <ajuhos@logiscool.com>
2019-04-18 17:14:51 +0200

    Updates

--- dist/index.js
+++ dist/index.js
@@ -1,11 +1,78 @@
 "use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
 const api_core_1 = require("api-core");
 const api_model_mongoose_1 = require("api-model-mongoose");
 const api_provider_ellipse_1 = require("api-provider-ellipse");
 const mongoose = require("mongoose");
-const Ellipse = require('ellipse'), app = new Ellipse;
+const Ellipse = require('ellipse'), ACL = require('acl'), app = new Ellipse;
+let acl = new ACL(new ACL.memoryBackend());
+acl.allow("admin", "student", "*");
+acl.allow("admin", "school", "Get");
+acl.addUserRoles("5812832682285d4a08d67eb9", "admin");
 mongoose.Promise = global.Promise;
 mongoose.connect("mongodb://localhost/api-demo");
+function mapStudentEntry(user, student) {
+    let temp = {
+        id: student._id.toHexString(),
+        name: [student.firstName, student.lastName].join(' '),
+        school: student.schoolId
+    };
+    if (student.classId)
+        temp['class'] = student.classId;
+    if (user.id === temp.id) {
+        temp.email = student.email;
+        temp.phone = student.phone;
+    }
+    return temp;
+}
+function mapStudent(scope) {
+    return new Promise(resolve => {
+        if (scope.response) {
+            const data = scope.response.data;
+            const mapper = mapStudentEntry.bind(null, scope.identity);
+            scope.response.data = Array.isArray(data) ? data.map(mapper) : mapper(data);
+        }
+        resolve(scope);
+    });
+}
+function ensureAuthenticated(scope) {
+    return new Promise((resolve, reject) => {
+        if (scope.identity)
+            resolve(scope);
+        else
+            reject(new api_core_1.ApiEdgeError(403, "Forbidden"));
+    });
+}
+function ensureACL(scope) {
+    return new Promise((resolve, reject) => {
+        let querySteps = scope.query.steps
+            .filter((step) => step instanceof api_core_1.QueryEdgeQueryStep);
+        function next() {
+            const step = querySteps.pop();
+            acl.isAllowed(scope.identity.id, step.query.edge.name, api_core_1.ApiEdgeQueryType[step.query.type])
+                .then((allowed) => {
+                if (allowed) {
+                    if (querySteps.length) {
+                        next();
+                    }
+                    else {
+                        resolve(scope);
+                    }
+                }
+                else {
+                    reject(new api_core_1.ApiEdgeError(403, `Forbidden (${api_core_1.ApiEdgeQueryType[step.query.type]} ${step.query.edge.name})`));
+                }
+            })
+                .catch(reject);
+        }
+        if (querySteps.length) {
+            next();
+        }
+        else {
+            resolve(scope);
+        }
+    });
+}
 const studentEdge = api_model_mongoose_1.MongooseModelFactory.createModel("student", "students", {
     id: String,
     firstName: String,
@@ -17,7 +84,7 @@ const studentEdge = api_model_mongoose_1.MongooseModelFactory.createModel("stude
         type: mongoose.Schema.Types.ObjectId
     },
     classId: mongoose.Schema.Types.ObjectId
-}), classEdge = api_model_mongoose_1.MongooseModelFactory.createModel("class", "classes", {
+}).action("map", mapStudent, api_core_1.ApiEdgeQueryType.Any, api_core_1.ApiEdgeActionTriggerKind.AfterEvent, api_core_1.ApiEdgeActionTrigger.OutputQuery), classEdge = api_model_mongoose_1.MongooseModelFactory.createModel("class", "classes", {
     id: String,
     name: String,
     semester: String,
@@ -55,9 +122,15 @@ const api11 = new api_core_1.Api('1.1')
     .relation(new api_core_1.OneToManyRelation(classEdge, studentEdge))
     .relation(new api_core_1.OneToManyRelation(classEdge, courseEdge))
     .relation(new api_core_1.OneToManyRelation(schoolEdge, studentEdge))
-    .relation(new api_core_1.OneToManyRelation(schoolEdge, classEdge));
+    .relation(new api_core_1.OneToManyRelation(schoolEdge, classEdge))
+    .action("authenticate", ensureAuthenticated)
+    .action("acl", ensureACL);
 app.use(require('body-parser').json());
 app.get('/favicon.ico', (req, res) => res.send(''));
+app.use(function (req, res, next) {
+    req.user = { id: "5812832682285d4a08d67eb9" };
+    next();
+});
 const router = new api_provider_ellipse_1.EllipseApiRouter(api11, api10);
 router.apply(app);
 app.listen(8080);
--- index.ts
+++ index.ts
@@ -1,14 +1,92 @@
-import {ApiEdgeError, OneToOneRelation, OneToManyRelation, ApiEdgeQueryResponse, Api} from "api-core";
+import {
+    ApiQueryScope, ApiEdgeError, OneToOneRelation, OneToManyRelation, ApiEdgeQueryResponse, Api, ApiEdgeQueryType,
+    ApiEdgeActionTriggerKind, ApiEdgeActionTrigger, QueryEdgeQueryStep
+} from "api-core";
 import {MongooseModelFactory} from "api-model-mongoose";
 import {EllipseApiRouter} from "api-provider-ellipse";
 import * as mongoose from "mongoose";
 
-
 const Ellipse = require('ellipse'),
+      ACL = require('acl'),
       app = new Ellipse;
+
+let acl = new ACL(new ACL.memoryBackend());
+acl.allow("admin", "student", "*");
+acl.allow("admin", "school", "Get");
+acl.addUserRoles("5812832682285d4a08d67eb9", "admin");
 
 mongoose.Promise = global.Promise;
 mongoose.connect("mongodb://localhost/api-demo");
+
+function mapStudentEntry(user: any, student: any) {
+    let temp: any = {
+        id: student._id.toHexString(),
+        name: [student.firstName, student.lastName].join(' '),
+        school: student.schoolId
+    };
+
+    if(student.classId)
+        temp['class'] = student.classId;
+
+    if(user.id === temp.id) {
+        temp.email = student.email;
+        temp.phone = student.phone
+    }
+
+    return temp
+}
+
+function mapStudent(scope: ApiQueryScope) {
+    return new Promise(resolve => {
+        if(scope.response) {
+            const data = scope.response.data;
+            const mapper = mapStudentEntry.bind(null, scope.identity);
+            scope.response.data = Array.isArray(data) ? data.map(mapper) : mapper(data)
+        }
+
+        resolve(scope)
+    })
+}
+
+function ensureAuthenticated(scope: ApiQueryScope) {
+    return new Promise((resolve, reject) => {
+        if(scope.identity) resolve(scope);
+        else reject(new ApiEdgeError(403, "Forbidden"))
+    })
+}
+
+function ensureACL(scope: ApiQueryScope) {
+    return new Promise((resolve, reject) => {
+        let querySteps = scope.query.steps
+            .filter((step: any) => step instanceof QueryEdgeQueryStep);
+
+        function next() {
+            const step: QueryEdgeQueryStep = querySteps.pop() as QueryEdgeQueryStep;
+            acl.isAllowed(scope.identity.id, step.query.edge.name, ApiEdgeQueryType[step.query.type])
+                .then((allowed: boolean) => {
+                    if (allowed) {
+                        if (querySteps.length) {
+                            next()
+                        }
+                        else {
+                            resolve(scope)
+                        }
+                    }
+                    else {
+                        reject(new ApiEdgeError(403, `Forbidden (${ApiEdgeQueryType[step.query.type]} ${step.query.edge.name})`))
+                    }
+                })
+                .catch(reject);
+        }
+
+        if(querySteps.length) {
+            next()
+        }
+        else {
+            resolve(scope)
+        }
+    })
+}
 
 const studentEdge =
           MongooseModelFactory.createModel("student", "students", {
@@ -22,7 +100,10 @@ const studentEdge =
                   type: mongoose.Schema.Types.ObjectId
               },
               classId: mongoose.Schema.Types.ObjectId
-          }),
+          }).action("map", mapStudent,
+              ApiEdgeQueryType.Any,
+              ApiEdgeActionTriggerKind.AfterEvent,
+              ApiEdgeActionTrigger.OutputQuery),
       classEdge =
           MongooseModelFactory.createModel("class", "classes", {
               id: String,
@@ -72,10 +153,18 @@ const api11
     .relation(new OneToManyRelation(classEdge, studentEdge))
     .relation(new OneToManyRelation(classEdge, courseEdge))
     .relation(new OneToManyRelation(schoolEdge, studentEdge))
-    .relation(new OneToManyRelation(schoolEdge, classEdge));
+    .relation(new OneToManyRelation(schoolEdge, classEdge))
+    .action("authenticate", ensureAuthenticated)
+    .action("acl", ensureACL);
+
 
 app.use(require('body-parser').json());
 app.get('/favicon.ico', (req: any, res: any) => res.send(''));
+
+app.use(function(req: any, res: any, next: any) {
+    req.user = { id: "5812832682285d4a08d67eb9" };
+    next()
+});
 
 const router = new EllipseApiRouter(api11, api10);
 router.apply(app);
